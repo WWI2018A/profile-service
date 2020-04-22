0859682c0d8013aa00e756abeec1ef683351dd22
Daniel Schönfeld <50236905+danielschoenfeld@users.noreply.github.com>
2020-04-22 21:10:06 +0200

    changes

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv').config();
 // -- Configure cloudinary. ----------------------------------
 cloudinary.config ({
     
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    cloud_name: 997566815525513,//process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: "7Ao-q3gVUlvGKcwAuUGwMO8lqX4",//process.env.CLOUDINARY_API_KEY,
+    api_secret: "thewebsitemediacloud"//process.env.CLOUDINARY_API_SECRET
 });
